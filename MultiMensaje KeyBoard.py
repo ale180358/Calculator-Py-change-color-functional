cb56ff631e1e497e3c15df480446fbdbf51bbe2e
alexander180358 <alexander22618@gmail.com>
2023-10-26 12:43:10 -0300

    Se agrega el Proyecto

---
+++ MultiMensaje KeyBoard.py
@@ -0,0 +1,41 @@
+hhttp:/ttp://Wa.me/settings
+http://Wa.me/settings
+/Wa.me/settings
+http://Wa.me/settings
+ttp:/http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/shttp://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+/Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+http://Wa.me/settings
+import keyboard
+import time
+import threading
+
+def  enviar():
+ while(True):
+    time.sleep(0.3)
+    keyboard.write("http://Wa.me/settings")
+    keyboard.press_and_release("enter")
+     
+time.sleep(5)   
+hilo = threading.Thread(target=enviar())
+hilo.start()
+             
+      
+
